d77e93fcc91892825e88ea22608b777b77f0577d
whoany <120975118+whoany@users.noreply.github.com>
2026-05-12 19:35:22 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,7 +1,7 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
- 
+
 template<typename T>
 class TPQueue {
  private:
@@ -10,7 +10,7 @@ class TPQueue {
     Node* next;
   };
   Node* head;
- 
+
  public:
   TPQueue() : head(nullptr) {}
  
@@ -21,7 +21,7 @@ class TPQueue {
       delete tmp;
     }
   }
- 
+
   void push(const T& item) {
     Node* newNode = new Node{item, nullptr};
 
@@ -38,7 +38,7 @@ class TPQueue {
     newNode->next = cur->next;
     cur->next = newNode;
   }
- 
+
   T pop() {
     T value = head->value;
     Node* tmp = head;
@@ -47,10 +47,10 @@ class TPQueue {
     return value;
   }
 };
- 
+
 struct SYM {
   char ch;
   int prior;
 };
- 
+
 #endif  // INCLUDE_TPQUEUE_H_
